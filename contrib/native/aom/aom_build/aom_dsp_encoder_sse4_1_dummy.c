65290deea7d0c635d5d032b2fb4d9def80cca5f9
Boogiedas <das.sanctity.ds@gmail.com>
2018-06-13 13:51:06 -0700

    Revert "Close button removed from UI"
    This reverts commit cd867d56aa2e2cf25e9dc48f1704b99f0a006f5e.

--- contrib/native/aom/aom_build/aom_dsp_encoder_sse4_1_dummy.c
+++
@@ -1,4 +0,0 @@
-// Generated file. DO NOT EDIT!
-// aom_dsp_encoder_sse4_1 needs a c file to force link language, 
-// or to silence a harmless CMake warning: Ignore me.
-void aom_dsp_encoder_sse4_1_dummy_function(void) {}
--- contrib/native/libplacebo/src/tests/context.c
+++
@@ -1,7 +0,0 @@
-#include "tests.h"
-
-int main()
-{
-    struct pl_context *ctx = pl_test_context();
-    pl_context_destroy(&ctx);
-}
